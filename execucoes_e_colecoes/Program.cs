ac2cebd9a06d19dacc1b4f9c55dd05f9a6e944e9
Lucas-M7 <lucas015x@gmail.com>
2024-01-02 11:18:31 -0300

    Adicionando a pasta exceções e coleções

---
+++ execucoes_e_colecoes/Program.cs
@@ -0,0 +1,24 @@
+﻿try
+{
+    string[] linhas = File.ReadAllLines("Arquivos/pasta/arquivoLeitura.txt");
+
+    foreach (string linha in linhas)
+    {
+        Console.WriteLine(linha);
+    }
+}catch(FileNotFoundException ex)
+{
+    Console.WriteLine($"Arquivo não encontrado. {ex.Message}");
+}
+catch(DirectoryNotFoundException ex)
+{
+    Console.WriteLine($"Diretório não encontrado. {ex.Message}");
+}
+catch(Exception ex)
+{
+    Console.WriteLine($"Ocorreu uma execção genéria. {ex.Message}");
+}
+finally
+{
+    Console.WriteLine("Processo finalizado");
+}
