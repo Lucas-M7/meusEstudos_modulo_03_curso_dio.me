5014336611b0ac723e8a5c71fbecf9fb63c13d65
Lucas-M7 <lucas015x@gmail.com>
2024-01-04 18:18:14 -0300

    Adicionando a pasta sobre If Ternário

---
+++ ifTernario/Program.cs
@@ -0,0 +1,24 @@
+﻿int myNumber = 15;
+bool ehPar = true;
+
+ehPar = myNumber % 2 == 0;
+
+Console.WriteLine($"O número {myNumber} é " + (ehPar ? "par" : "ímpar"));
+
+
+
+
+
+
+
+
+
+// if(myNumber % 2 == 0)
+// {
+//     Console.WriteLine("Número par");
+// }
+// else
+// {
+//     Console.WriteLine("Número ímpar");
+// }
+
