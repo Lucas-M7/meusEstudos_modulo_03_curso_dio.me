9cf0affe6a09b41341e6dd859330081dae3171a7
Lucas-M7 <lucas015x@gmail.com>
2024-01-05 10:53:01 -0300

    Adicionando a pasta sobre Gerenciador de Pacotes

---
+++ gerenciadorDePacotes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.Design;
+using System.Text;
+using gerenciadorDePacotes.Models;
+using Newtonsoft.Json;
+
+Vendas v1 = new Vendas(7, "Pen-Drive", 27.00M);
+
+string serializado = JsonConvert.SerializeObject(v1, Formatting.Indented);
+
+// File.WriteAllText("Arquivos/vendas.json", serializado);
+
+Console.WriteLine(serializado);
