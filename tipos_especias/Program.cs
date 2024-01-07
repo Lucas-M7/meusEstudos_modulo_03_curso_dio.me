019cc42fb7fde54fe1514a7d62ce89700f4c69e8
Lucas-M7 <lucas015x@gmail.com>
2024-01-07 12:04:28 -0300

    adicionando a pasta sobre tipos especiais

---
+++ tipos_especias/Program.cs
@@ -0,0 +1,11 @@
+﻿bool? desejaReceberEmails = null;
+
+    //Verifica se tem um valor   |  Verifica se o valor é verdadeiro
+if (desejaReceberEmails.HasValue && desejaReceberEmails.Value)
+{
+    Console.WriteLine("Vai receber");
+}
+else
+{
+    Console.WriteLine("O user não respondeu ou optou por não receber");
+}
